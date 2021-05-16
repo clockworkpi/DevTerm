bcc984109afe87ded727f71c3695563e33535d0a
cuu <gnubsd@me.com>
2021-05-16 13:51:52 +0800

    tab to be two space in PRINT_STATE

--- Code/thermal_printer/devterm_thermal_printer.c
+++ Code/thermal_printer/devterm_thermal_printer.c
@@ -682,7 +682,7 @@ void loop() {
       {	
         fread(readbuf,1, 1,fp);
         //printf("read %x",readbuf[0]);
-        if(cfg->state ==  PRINT_STATE) {
+        if(g_config.state ==  PRINT_STATE) {
         	if(readbuf[0] == ASCII_TAB) {
         		readbuf[0] = ' ';
         		parse_serial_stream(&g_config,readbuf[0]);
